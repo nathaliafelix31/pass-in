2d6ef18363e5563b8cb52372e68ec1573f2c14fb
nathaliafelix31 <nathy.felix.31@gmail.com>
2024-04-04 17:52:46 -0300

    NLW aula 2

---
+++ src/main/java/rocketseat/com/passin/dto/event/EventDetailDTO.java
@@ -0,0 +1,7 @@
+package rocketseat.com.passin.dto.event;
+
+public record EventDetailDTO(String id, String title,
+                             String details, String slug,
+                             Integer maximumAttendees,
+                             Integer attendeesAmount) {
+}
